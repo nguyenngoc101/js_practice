8aff9898a5bb4cff44715367f9d42c94596967ef
ngocnv <nguyenngoc101@yahoo.com>
2016-03-06 14:50:06 +0700

    memoize

--- memoize/index.js
+++ memoize/index.js
@@ -1,11 +1,13 @@
 function memoize(fn) {
   var called = 0;
-  var result;
+  var result = [];
   return function() {
     var cxt = this;
     var args = [].slice.call(arguments);
-    if (called == 0) return (result=fn.apply(cxt, args));
-    else return result;
+    if (!result[args]) {
+      result[args] = fn.apply(cxt, args);
+    }
+    return result[args];
   };
 }
 
